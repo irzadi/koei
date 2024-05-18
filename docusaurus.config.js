100a59a3a52bd858ec10e02436f701bdff3cb88d
Irzadi Siregar <irzadi@gmail.com>
2024-05-18 17:51:34 +0700

    change site name to Tiaramas

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Tiaramas Knowledge Base',
+  tagline: 'Connect People, Realize Dreams',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -66,9 +66,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Tiaramas Knowledge Base',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Tiaramas Logo',
           src: 'img/logo.svg',
         },
         items: [
